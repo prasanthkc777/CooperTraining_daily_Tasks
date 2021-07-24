13630f34273929b52cde0c107b6a310d668c2808
prasanthkc777 <83226287+prasanthkc777@users.noreply.github.com>
2021-07-24 19:58:57 +0530

    Update Maximum_stamina.py

--- Day_9/Maximum_stamina.py
+++ Day_9/Maximum_stamina.py
@@ -1,1 +1,18 @@
 
+myinp=int(input())
+arr=list(map(int,input().split()))
+myarr=[]
+for i in range(myinp-1):
+    checkmax=arr[i]
+    eval_out=str(arr[i])
+    for j in range(i+1,myinp):
+        if arr[j]>checkmax:
+            checkmax=arr[j]
+            eval_out+='^'+str(arr[j])
+    
+    myarr.append(eval(eval_out))
+
+
+myarr.append(arr[-1])
+
+print(max(myarr))
